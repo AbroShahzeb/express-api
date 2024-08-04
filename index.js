1535fd99059ba04140022841b7c180604958eb11
AbroShahzeb <khizr77@gmail.com>
2024-08-03 21:22:43 -0700

    Added /user route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res, next) => {
     res.json({message: 'Hello world'})
 })
 
+app.get('/user', (req, res, next) => {
+    res.json({id: 1, name: 'Shahzeb Abro', email: 'shahzeb@gmail.com'})
+})
+
 console.log('Hello world')
 
 app.listen(3000, () => console.log('App is listening on port 3000'))
