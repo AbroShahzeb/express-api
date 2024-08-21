6b820d8c9cc923c50ba54b61623943478a4f3a51
AbroShahzeb <khizr77@gmail.com>
2024-08-21 01:40:26 -0700

    Implemented mail

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,11 @@ import express from 'express';
 const router = express.Router();
 
 import { login, googleAuth, register } from '../controllers/authController.js';
+import { sendEmail } from '../utils/email.js';
 
 router.post('/login', login);
 router.post('/register', register);
 router.post('/google', googleAuth);
+router.post('/mail', sendEmail);
 
 export default router;
---
+++ utils/email.js
@@ -0,0 +1,60 @@
+import nodemailer from 'nodemailer';
+import Mailgen from 'mailgen';
+import catchAsync from './catchAsync.js';
+import AppError from './appError.js';
+
+export const sendEmail = catchAsync(async (req, res, next) => {
+    const transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    let MailGenerator = new Mailgen({
+        theme: 'default',
+        product: {
+            name: 'Mailgen',
+            link: 'https://mailgen.js',
+        },
+    });
+
+    let response = {
+        body: {
+            name: 'Shahzeb',
+            intro: 'Your bill has arrived',
+            table: {
+                data: [
+                    {
+                        item: 'Nodemailer',
+                        description: 'Your purchase has been successful',
+                        price: '$10.99',
+                    },
+                ],
+            },
+            outro: 'Looking forward to do more business',
+        },
+    };
+
+    let mail = MailGenerator.generate(response);
+
+    let message = {
+        from: process.env.EMAIL,
+        to: 'khizr772@gmail.com',
+        subject: 'Order placement',
+        html: mail,
+    };
+
+    transport
+        .sendMail(message)
+        .then(() => {
+            return res.status(201).json({
+                msg: 'you should receive an email',
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+            return next(new AppError('Mail could not be sent'));
+        });
+});
